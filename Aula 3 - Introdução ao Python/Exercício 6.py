132889409dc4f56a0be423021f744a68445b9b7c
Camile Stefany <131724367+camsste@users.noreply.github.com>
2024-04-08 15:33:02 -0300

    Create Exercício 6.py

---
+++ Aula 3 - Introdução ao Python/Exercício 6.py
@@ -0,0 +1,27 @@
+mes = int(input("Coloque um número de 1 à 12 para ter o mês do ano correspondente: "))
+if mes == 1:
+   print(f"O mês é Janeiro")
+if mes == 2:
+   print(f"O mês é Fevereiro")
+if mes == 3:
+   print(f"O mês é Março")
+if mes == 4:
+   print(f"O mês é Abril")
+if mes == 5:
+   print(f"O mês é Maio")
+if mes == 6:
+   print(f"O mês é Junho")
+if mes == 7:
+   print(f"O mês é Julho")
+if mes == 8:
+   print(f"O mês é Agosto")
+if mes == 9:
+   print(f"O mês é Setembro")
+if mes == 10:
+   print(f"O mês é Outubro")
+if mes == 11:
+   print(f"O mês é Novembro")
+if mes == 12:
+   print(f"O mês é Dezembro")
+if mes > 12 or mes < 1:
+   print(f"O número inserido não é válido")
