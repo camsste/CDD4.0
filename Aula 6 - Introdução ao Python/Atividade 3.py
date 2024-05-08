0d7dd4a6b2d02f9fe354d92661f250eaaf27a8e3
Camile Stefany <131724367+camsste@users.noreply.github.com>
2024-05-08 17:00:13 -0300

    Delete Aula 6 - Introdução ao Python directory

--- Aula 6 - Introdução ao Python/Atividade 3.py
+++
@@ -1,9 +0,0 @@
-print(f"Vamos descobrir quanto tempo durou o jogo! Vamos lá?")
-inicio = int(input("Digite aqui a hora que iniciou o jogo de xadrês:"))
-fim = int(input("Digite aqui a hora que terminou:"))
-
-if inicio <= fim:
-    duracao = fim - inicio
-else:
-    duracao = 24 - inicio + fim
-print(f"O jogo durou {duracao} horas.")
