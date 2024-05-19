58af28e4cabacfd130e7ad9355060a13ee3f0f1f
Camile Stefany <131724367+camsste@users.noreply.github.com>
2024-05-19 00:35:29 -0300

    Add files via upload

---
+++ Desafios/Desafio Avançado/main.py
@@ -0,0 +1,10 @@
+from classes import *
+
+p1 = Pessoa("Ebony", "8kg", 21, False, False, False)
+p2 = Pessoa("Ariana", 45, 28,  False, False, False)
+p3 = Pessoa("Mc Tróia", 40, 32,  False, False, False)
+p4 = Pessoa("Felipe Neto", 65, 37,  False, False, False)
+
+p1.pararComer()
+p1.dormir("8 horas")
+p1.pararDormir()
